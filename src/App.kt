cf0b445711f58f2ebad4acdbb8fc0402df573175
aparcerozas <vahperi98>
2019-06-06 10:15:27 +0200

    Main Examples

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,22 @@
 fun main() {
+    var s: Seleccion = Seleccion(1,"Pedro")
+    var x: Xogador = Xogador(7, "delantero", 2, "Cristiano")
+    var a: Seleccion = Adestrador(3, "Zinedine")
+    var m: Masaxista = Masaxista("experto", 15, 4, "Marta")
+
+    s.concentrarse()
+    s.viaxar()
+
+    x.concentrarse()
+    x.viaxar()
+
+    println(a.toString())
+    println(m.toString())
+
+    a.nome = "Zidane"
+    m.id = 7
+
+    println(a.nome)
+    println(m.id)
 
 }
